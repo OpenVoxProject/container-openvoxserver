93eb9644a2d9b70e4ebd7dbbf399c037d7a54e54
Robert Waffen <rwaffen@gmail.com>
2025-01-17 16:53:28 +0100

    fix: remove path from binary

--- openvoxserver/docker-entrypoint.d/50-set-certname.sh
+++ openvoxserver/docker-entrypoint.d/50-set-certname.sh
@@ -3,9 +3,9 @@
 set -e
 
 if [ -n "${OPENVOXSERVER_HOSTNAME}" ]; then
-  /usr/bin/puppet config set server "$OPENVOXSERVER_HOSTNAME"
+  puppet config set server "$OPENVOXSERVER_HOSTNAME"
 fi
 
 if [ -n "${CERTNAME}" ]; then
-  /usr/bin/puppet config set certname "$CERTNAME"
+  puppet config set certname "$CERTNAME"
 fi
