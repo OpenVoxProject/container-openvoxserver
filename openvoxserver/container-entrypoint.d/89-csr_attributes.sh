68c09d4340ddde25c2be2ddeeb3b4ac8f019d481
Sebastian Maier <sebastian.maier@siemens.com>
2025-07-25 08:10:21 +0200

    refactor: rename docker-entrypoint.{d,sh} to container-entrypoint.{d,sh}; adjust scripts and readme
    Signed-off-by: Sebastian Maier <sebastian.maier@siemens.com>

--- openvoxserver/container-entrypoint.d/89-csr_attributes.sh
+++ openvoxserver/container-entrypoint.d/89-csr_attributes.sh
@@ -4,5 +4,5 @@ set -e
 
 if [ -n "${CSR_ATTRIBUTES}" ]; then
     echo "CSR Attributes: ${CSR_ATTRIBUTES}"
-    /opt/puppetlabs/puppet/bin/ruby /docker-entrypoint.d/89-csr_attributes.rb
+    /opt/puppetlabs/puppet/bin/ruby /container-entrypoint.d/89-csr_attributes.rb
 fi
