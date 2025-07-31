577f134b087c29b0265560cc37ef59099da0b5b4
Sebastian Maier <sebastian.maier@siemens.com>
2025-07-31 11:16:15 +0200

    refactor: avoid hard-coded file paths
    Signed-off-by: Sebastian Maier <sebastian.maier@siemens.com>

--- openvoxserver/container-entrypoint.d/89-csr_attributes.sh
+++ openvoxserver/container-entrypoint.d/89-csr_attributes.sh
@@ -2,7 +2,11 @@
 
 set -e
 
+# determine script location
+readonly SCRIPT_FILENAME=$(readlink -f "${BASH_SOURCE[0]}")
+readonly SCRIPT_PATH=$(dirname "$SCRIPT_FILENAME")
+
 if [ -n "${CSR_ATTRIBUTES}" ]; then
     echo "CSR Attributes: ${CSR_ATTRIBUTES}"
-    /opt/puppetlabs/puppet/bin/ruby /container-entrypoint.d/89-csr_attributes.rb
+    /opt/puppetlabs/puppet/bin/ruby "$SCRIPT_PATH/89-csr_attributes.rb"
 fi
